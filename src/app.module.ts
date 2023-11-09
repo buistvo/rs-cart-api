d7565b70fff608030cd64ddef513504b434caaca
buistvo <plus100500@gmail.com>
2023-11-09 13:20:00 +0300

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,23 @@ import { AppController } from './app.controller';
 import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     AuthModule,
     CartModule,
     OrderModule,
+      TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
   ],
   controllers: [
     AppController,
